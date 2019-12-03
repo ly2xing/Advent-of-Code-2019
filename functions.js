66a7d1fd1e5678a66108f54f42804793dc66304a
Ling Xing <github@lxing.ca>
2019-12-03 11:27:16 -0500

    day3_1

--- functions.js
+++ functions.js
@@ -1,5 +1,70 @@
 module.exports = {
-  day2_2: (number) => {
+  day3_1: (json) => {
+    const [line1Moves, line2Moves] = json;
+    const makeMove = (coordinates, move) => {
+      const operator = move.substring(0, 1);
+      const distance = move.substring(1);
+      let points = new Array(+distance).fill(1);
+      switch (operator) {
+        case 'U':
+          coordinates.y += +distance;
+          points = points.map((val, index) => {
+            const coordinate = {...coordinates};
+            coordinate.y -= (+distance - index - 1);
+            return coordinate;
+          });
+          break;
+        case 'R':
+          coordinates.x += +distance;
+          points = points.map((val, index) => {
+            const coordinate = {...coordinates};
+            coordinate.x -= (+distance - index - 1);
+            return coordinate;
+          });
+          break;
+        case 'D':
+          coordinates.y -= +distance;
+          points = points.map((val, index) => {
+            const coordinate = {...coordinates};
+            coordinate.y += +distance - index - 1;
+            return coordinate;
+          });
+          break;
+        case 'L':
+          coordinates.x -= +distance;
+          points = points.map((val, index) => {
+            const coordinate = {...coordinates};
+            coordinate.x += +distance - index - 1;
+            return coordinate;
+          });
+          break;
+        default:
+          console.error(`Unknown move, current coordinates:`, coordinates);
+      }
+      return points;
+    };
+    const generateLine = (moves) => {
+      const coordinates = {x: 0, y: 0};
+      let line = [];
+      for (let move of moves) {
+        const points = makeMove(coordinates, move);
+        line = line.concat(points);
+      }
+      return line;
+    };
+    const comparator = (a, b) => (Math.abs(a.x) + Math.abs(a.y)) < (Math.abs(b.x) + Math.abs(b.y)) ? -1 : 1;
+    const line1 = generateLine(line1Moves).sort(comparator);
+    const line2 = generateLine(line2Moves).sort(comparator);
+    const line1Strings = line1.map(l => `X${l.x}Y${l.y}`);
+    const line2Strings = line2.map(l => `X${l.x}Y${l.y}`);
+    const intersections = line1Strings.filter(value => line2Strings.includes(value));
+    console.log(intersections);
+    const yIndex = intersections[0].indexOf('Y');
+    const x = +intersections[0].substring(1, yIndex);
+    const y = +intersections[0].substring(yIndex + 1);
+    return Math.abs(x) + Math.abs(y);
+  },
+  day2_2: (json, number) => {
     if (number === undefined) {
       number = 19690720;
     }
@@ -7,7 +72,7 @@ module.exports = {
     let verb = 0;
     let output = -1;
     while (output !== number) {
-      const code = require('./input2_2.json').slice(0);
+      const code = json.slice(0);
       noun++;
       if (noun > 99) {
         noun = 0;
@@ -19,10 +84,8 @@ module.exports = {
     }
     return {noun, verb};
   },
-  day2_1: (code) => {
-    if (!code) {
-      code = require('./input2_1.json');
-    }
+  day2_1: (json) => {
+    const code = json;
     let currentLine = 0;
     const getIndex = (lineNumber, instruction) => lineNumber * 4 + instruction;
     const readLine = (lineNumber) => [0, 1, 2, 3].map(ins => code[getIndex(lineNumber, ins)]);
@@ -50,24 +113,22 @@ module.exports = {
     }
     return code[0];
   },
-  day1_2: () => {
-    const json = require('./input1_2.json');
-    const calculateFuel = fuel => Math.max(0, Math.floor(fuel / 3) - 2);
+  day1_2: (json) => {
+    const calculateFuel = fuel => {
+      let fuelNeeded = Math.max(0, Math.floor(fuel / 3) - 2);
+      if (fuelNeeded > 0) {
+        fuelNeeded += calculateFuel(fuelNeeded);
+      }
+      return fuelNeeded;
+    };
     const calculateFuels = (fuels) => {
-      return fuels.flatMap(fuel => {
-        const list = [];
-        let input = fuel;
-        while (input > 0) {
-          input = calculateFuel(input);
-          list.push(input);
-        }
-        return list;
+      return fuels.map(fuel => {
+        return calculateFuel(fuel);
       });
     };
     return calculateFuels(json).reduce((sum, input) => sum + input);
   },
-  day1_1: () => {
-    const json = require('./input1_1.json');
+  day1_1: (json) => {
     return json.reduce((sum, input) => sum + (Math.floor(input / 3) - 2), 0);
   }
 };
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const functions = require('./functions');
 const keys = Object.keys(functions).sort((a, b) => a < b ? -1 : (a > b ? 1 : 0));
 for (let key of keys) {
+  console.info('========================');
+  console.info(`Executing ${key}`);
+  const func = functions[key];
+  const json = require(`./data/input${key.substring(3)}.json`);
   const startTime = process.hrtime();
-  const result = functions[key]();
+  const result = func(json);
   const endTime = process.hrtime(startTime);
   console.info(`${key} completed, result:`, result);
   console.info('Execution time (hr): %ds %dms', endTime[0], endTime[1] / 1000000);
+  console.info('========================');
 }
